87e232ccdedf9c2590b673e98933b16efa78d970
Thomas Stewart <thomas.stewart0494@gmail.com>
2020-12-21 18:12:24 +0200

    Abstracting code

---
+++ src/Renderer.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Thomas Stewart on 2020/12/21.
+//
+
+#include "Renderer.h"
+
+#include <iostream>
+
+void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogCall(const char *function, const char *file, int line) {
+    while (auto error = glGetError()) {
+        std::cout << "[OpenGL Error] (" << error << "): " << function <<
+                  " " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
