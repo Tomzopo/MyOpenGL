c07038f930c38c08d28cb46f4cc6e3ae3062f519
Thomas Stewart <thomas.stewart0494@gmail.com>
2021-01-07 19:11:42 +0200

    Abstracted Textures

--- include/Shader.h
+++ include/Shader.h
@@ -17,7 +17,7 @@ class Shader {
 private:
     std::string m_FilePath;
     unsigned int m_RendererID;
-    std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+    std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
     Shader(const std::string &filepath);
     ~Shader();
@@ -26,6 +26,7 @@ public:
     void Unbind() const;
 
     // Set uniforms
+    void SetUniform1i(const std::string &name, int value);
     void SetUniform1f(const std::string &name, float value);
     void SetUniform4f(const std::string &name, float v0, float v1, float f2, float f3);
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <fstream>
-#include <string>
-#include <sstream>
 
 #include "Renderer.h"
 
@@ -12,6 +9,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main() {
     GLFWwindow *window;
@@ -44,10 +42,10 @@ int main() {
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-                -0.5f, -0.5f,   // 0
-                0.5f, -0.5f,    // 1
-                0.5f, 0.5f,     // 2
-                -0.5f, 0.5f     // 3
+                -0.5f, -0.5f, 0.0f, 0.0f,  // 0
+                0.5f, -0.5f, 1.0f, 0.0f,   // 1
+                0.5f, 0.5f, 1.0f, 1.0f,     // 2
+                -0.5f, 0.5f, 0.0f, 1.0f,     // 3
         };
 
         unsigned int indices[] = {
@@ -55,10 +53,14 @@ int main() {
                 2, 3, 0
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
@@ -67,6 +69,10 @@ int main() {
         Shader shader("../res/shaders/Basics.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+
+        Texture texture("res/textures/Logo.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
 
         va.Unbind();
         vb.Unbind();
