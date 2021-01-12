940e77cda4bbd80b8906392df63871d94d945781
Thomas Stewart <thomas.stewart0494@gmail.com>
2021-01-12 17:25:32 +0200

    MVP maths

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "OpenGL Tutorial", nullptr, nullptr);
+    window = glfwCreateWindow(1920, 1080, "OpenGL Tutorial", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -45,10 +45,10 @@ int main() {
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-                -0.5f, -0.5f, 0.0f, 0.0f,  // 0
-                0.5f, -0.5f, 1.0f, 0.0f,   // 1
-                0.5f, 0.5f, 1.0f, 1.0f,     // 2
-                -0.5f, 0.5f, 0.0f, 1.0f,     // 3
+                100.0f, 100.0f, 0.0f, 0.0f,  // 0
+                200.0f, 100.0f, 1.0f, 0.0f,   // 1
+                200.0f, 200.0f, 1.0f, 1.0f,     // 2
+                100.0f, 200.0f, 0.0f, 1.0f,     // 3
         };
 
         unsigned int indices[] = {
@@ -69,12 +69,15 @@ int main() {
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("../res/shaders/Basics.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("../res/textures/Logo.png");
         texture.Bind();
